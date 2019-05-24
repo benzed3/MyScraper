47e96063abf6c1dac3cf96f4bd5689a602123b27
benzed3 <benzed3@gmail.com>
2019-05-24 15:10:54 -0400

    more changes to app.js

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -14,17 +14,21 @@ $.getJSON("/articles", function (data) {
 
 $(document).on("click", "#scrape", function () {
     alert("working");
-    $.getJSON("/scrape", function (data) {
+    $.ajax({
+        method: "GET",
+        url: "/scrape"
+    })
+        // With that done, add the note information to the page
+        .then(function (data) {
+            for (var i = 0; i < data.length; i++) {
 
-        for (var i = 0; i < data.length; i++) {
-
-            $("#articles").append("<p data-id='" +
-                data[i]._id + "'>" +
-                data[i].title + "<br />" +
-                data[i].link + "<br />" +
-                data[i].image + "<br />" +
-                data[i].summary +
-                "</p>");
-        }
-    });
+                $("#articles").append("<p data-id='" +
+                    data[i]._id + "'>" +
+                    data[i].title + "<br />" +
+                    data[i].link + "<br />" +
+                    data[i].image + "<br />" +
+                    data[i].summary +
+                    "</p>");
+            }
+        });
 });
