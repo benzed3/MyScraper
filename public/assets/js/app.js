48fd9a6be7da2eeb12d3ce3b595006a8107cd8f4
benzed3 <benzed3@gmail.com>
2019-05-24 14:49:22 -0400

    attempt to fix scraper button

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -14,44 +14,17 @@ $.getJSON("/articles", function (data) {
 
 $(document).on("click", "#scrape", function () {
     alert("working");
-    app.get("/scrape", function (req, res) {
+    .getJSON("/articles", function (data) {
 
-        axios.get("https://www.chess.com").then(function (response) {
+        for (var i = 0; i < data.length; i++) {
 
-            var $ = cheerio.load(response.data);
-
-
-            $("h2.post-preview-titlecontainer").each(function (i, element) {
-
-                var result = {};
-
-                result.title = $(this)
-                    .parent()
-                    .find("a")
-                    .attr("title");
-                result.link = $(this)
-                    .find("a")
-                    .attr("href");
-                result.image = $(this)
-                    .parent()
-                    .find("img")
-                    .attr("src");
-                result.summary = $(this)
-                    .parent()
-                    .find("p")
-                    .text().trim();
-
-                db.Article.create(result)
-                    .then(function (dbArticle) {
-
-                        console.log(dbArticle);
-                    })
-                    .catch(function (err) {
-
-                        console.log(err);
-                    });
-            });
-            res.send("Scrape Complete!");
-        });
+            $("#articles").append("<p data-id='" +
+                data[i]._id + "'>" +
+                data[i].title + "<br />" +
+                data[i].link + "<br />" +
+                data[i].image + "<br />" +
+                data[i].summary +
+                "</p>");
+        }
     });
 });
